d05cc2500221680ed705581f11cf0aeebaf588d6
AmirN347 <108190581+AmirN347@users.noreply.github.com>
2023-05-19 17:17:08 -0400

    Completed Counter with React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="container">
       <h1>code-next-react-counter</h1>
 
-      <h2>https://stackblitz.com/edit/code-next-react-counter</h2>
-
       <hr />
 
       <div>
-        {/* YOUR COUNTER CODE HERE */}
+        <h2> The Counter to beat all Counters!!</h2>
+        <h2> Count: {count} </h2>
+
+        <button className=" btn btn-primary" onClick={incrementCount}>
+          Increment
+        </button>
+
+        <button className="btn btn-primary m-2" onClick={decrementCount}>
+          {' '}
+          Decrement{' '}
+        </button>
       </div>
     </div>
   );
